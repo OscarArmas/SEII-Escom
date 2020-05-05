f784923796f0575aebe9402de4c95511b0129d7d
OscarArmas <saga_cth@hotmail.com>
2020-05-04 21:39:32 -0500

    Se agrego funcion de comprobacion Fullregister, para ver si un usuario requiere pre

---
+++ application/controllers/Preregister.php
@@ -0,0 +1,60 @@
+<<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+
+class Preregister extends CI_Controller
+{
+
+  function __construct()
+  {
+
+    parent::__construct();
+    $this->load->model('Mlogin');
+  }
+  public function index(){
+    $this->load->view('vregister');
+
+  }
+  public function fullregister(){
+    if(!$this->input->post()){
+			echo "Hicite algo mal!";
+			return;
+		}
+		$data = $this->input->post();
+    if(isset($data['nombre'])&&isset($data['boleta'])&&isset($data['appat'])&&isset($data['apmat'])&&isset($data['email'])&&isset($data['password'])&&isset($data['confirm-password'])){
+      $nombre = $data['nombre'];
+      $appat = $data['appat'];
+      $apmat = $data['apmat'];
+      $boleta = $data['boleta'];
+      $password = $data['password'];
+      $email = $data['email'];
+
+      $exist = $this->Mlogin->isFullRegister($boleta);
+      if($exist[0]->Correo == NULL){
+        print_r($data);
+        $this->Mlogin->Fullregister($nombre,$appat,$apmat,$email,$password,$boleta);
+      }
+      if($exist){
+        echo "El usuario o el email ya existe!";
+        return;
+    }
+
+
+  }
+
+
+
+
+}
+
+}
+
+
+
+
+
+
+
+
+
+ ?>
