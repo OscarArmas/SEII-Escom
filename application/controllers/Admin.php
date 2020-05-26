3f9ed29544b4f3b3ec47eae118409f6b2a4694ae
OscarArmas <saga_cth@hotmail.com>
2020-05-26 13:30:35 -0500

    COntroller para el admin

---
+++ application/controllers/Admin.php
@@ -0,0 +1,26 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Admin extends CI_Controller {
+  	function __construct()
+  	{
+  		parent::__construct();
+ 		$this->usuario = $this->session->userdata("user");
+		if(!$this->usuario){
+			redirect('dashboard');
+		}
+		if($this->usuario->nivel!=2){
+			redirect('dashboard');
+		}
+		#$this->load->model('musuario');
+		#$this->load->model('mescuela');
+		#$this->load->model('mevento');
+		#$this->load->library('form_validation');
+  	}
+
+	public function index(){
+
+		$this->load->view('Admin/vadmin');
+	}
+
+}
