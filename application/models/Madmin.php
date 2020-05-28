748d643e1a29b7f6d49240039adfc4cdd52cf43d
OscarArmas <saga_cth@hotmail.com>
2020-05-28 18:29:11 -0500

    controller para obtener alumnos

---
+++ application/models/Madmin.php
@@ -0,0 +1,17 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+/**
+*
+*/
+class Madmin extends CI_Model
+{
+  public function get_alumnos(){
+    $sql = $this->db->query("select Usuario_ID, Nombre
+    ,AppPaterno, AppMaterno, Boleta, Correo from Usuario")->result();
+
+    return json_encode($sql);
+  }
+
+
+
+
+}
