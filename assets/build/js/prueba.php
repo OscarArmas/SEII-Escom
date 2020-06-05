880e21c46ded85f9019bb32eb8bcdb6259c01718
Juan José Reyes Vilchis <maylo360xd@gmail.com>
2020-06-04 21:33:55 -0500

    Update prueba.php

--- assets/build/js/prueba.php
+++ assets/build/js/prueba.php
@@ -19,7 +19,7 @@ $Materias = array();
 if (mysqli_num_rows($result) > 0) {
   // output data of each row
   while($row = mysqli_fetch_assoc($result)) {
-                 $materia = array( $row["Materia_ID"],$row["Nombre"],$row["Nivel"],$row["Carrera_ID"]);
+                    $materia = array( $row["Materia_ID"],mb_convert_encoding($row["Nombre"],"UTF-8","iso-8859-1"),$row["Nivel"],$row["Carrera_ID"]);
                  array_push($Materias,$materia);
   }
   echo json_encode($Materias);
