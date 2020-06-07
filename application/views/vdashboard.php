ce98d0a4ed99af890a1daffadb7436c4d34d9db3
OscarArmas <saga_cth@hotmail.com>
2020-06-07 15:20:55 -0500

    botton de cerrar sesion

--- application/views/vdashboard.php
+++ application/views/vdashboard.php
@@ -88,16 +88,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
             <!-- /menu footer buttons -->
             <div class="sidebar-footer hidden-small">
-              <a data-toggle="tooltip" data-placement="top" title="Settings">
-                <span class="glyphicon glyphicon-cog" aria-hidden="true"></span>
-              </a>
-              <a data-toggle="tooltip" data-placement="top" title="FullScreen">
-                <span class="glyphicon glyphicon-fullscreen" aria-hidden="true"></span>
-              </a>
-              <a data-toggle="tooltip" data-placement="top" title="Lock">
-                <span class="glyphicon glyphicon-eye-close" aria-hidden="true"></span>
-              </a>
-              <a data-toggle="tooltip" data-placement="top" title="Logout" href="login.html">
+              <a data-toggle="tooltip" data-placement="top" title="Cerrar Sesion" href="login.html" style="width: 100%;">
                 <span class="glyphicon glyphicon-off" aria-hidden="true"></span>
               </a>
             </div>
